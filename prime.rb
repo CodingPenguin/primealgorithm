43b714506335749258155a32b91fa4cad9dd25ef
lephankoby <lephankoby@gmail.com>
2015-11-23 13:53:30 -0600

    Refactor from if...else to case...when

--- prime.rb
+++ prime.rb
@@ -1,14 +1,16 @@
 # Program Starts
-puts "Note: A negative, prime integer's absolute value is also prime\n(Enter a positive integer)"
 puts "Enter your number:"
 primeNumber = gets.chomp.to_i.abs
 
-if primeNumber == 1 || primeNumber == 0
+case
+when primeNumber == String
+  puts "Only Accept Number(s)"
+when primeNumber == 1, primeNumber == 0
   puts "Neither composite nor prime."
-elsif primeNumber == 2 || primeNumber == 3
+when primeNumber == 2, primeNumber == 3
   puts "#{primeNumber} is a prime number."
-elsif primeNumber % 2 == 0 || primeNumber % 3 == 0
-    puts "#{primeNumber} is a composite number."
+when primeNumber % 2 == 0, primeNumber % 3 == 0
+  puts "#{primeNumber} is a composite number."
 else
   puts "#{primeNumber} is a prime number."
 end
