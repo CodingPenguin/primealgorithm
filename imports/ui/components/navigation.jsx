4a68deda2707adfd4c8da776eda90b1b975be379
Vanielle Le <lephanvanielle@gmail.com>
2016-04-01 12:05:55 -0500

    Navigation UI

--- imports/ui/components/navigation.jsx
+++ imports/ui/components/navigation.jsx
@@ -11,17 +11,19 @@ import ActionHome from 'material-ui/lib/svg-icons/action/home'
 import ContentMail from 'material-ui/lib/svg-icons/content/mail'
 import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert'
 import ToolbarTitle from 'material-ui/lib/toolbar/toolbar-title'
+import Colors from 'material-ui/lib/styles/colors'
+
 
 export default class Navigation extends React.Component {
   render() {
     return (
-      <Toolbar>
+      <Toolbar style={{backgroundColor: "#EF5350"}}>
        <ToolbarGroup float="left">
-         <ToolbarTitle text="Prime Calculate"/>
+         <ToolbarTitle text="Prime Calculate" style={{color: "#fff"}}/>
          <FontIcon className="muidocs-icon-custom-sort"/>
        </ToolbarGroup>
        <ToolbarGroup float="right">
-          <IconMenu iconButtonElement={<IconButton touch={true}><MoreVertIcon/></IconButton>}>
+          <IconMenu iconButtonElement={<IconButton iconStyle={{fill: "white"}} touch={true}><MoreVertIcon/></IconButton>}>
             <MenuItem primaryText="Home" leftIcon={<ActionHome/>} href="/"/>
             <MenuItem primaryText="Contact" leftIcon={<ContentMail/>} href="/contact"/>
           </IconMenu>
